b52fdafcc1488e785785d6cb5b8a15a3db275ad0
liucw <yjwwyygy@163.com>
2015-08-31 15:26:24 +0800

    1、建立跨平台项目组织结构 2、添加错误处理代码文件

---
+++ src/basic/err.cpp
@@ -0,0 +1,26 @@
+#include "err.h"
+
+void safe_abort(const char *errMsg)
+{
+#ifdef WIN32
+	ULONG_PTR extra_info[1];
+	extra_info[0] = (ULONG_PTR)errMsg;
+	RaiseException(0x40000015, EXCEPTION_NONCONTINUABLE, 1, extra_info);
+#else
+	(void)errmsg_;
+	abort();
+#endif
+}
+
+#ifdef WIN32
+
+void win_error(char *buffer, size_t bufferSize)
+{
+	DWORD errcode = GetLastError();
+	DWORD rc = FormatMessageA(FORMAT_MESSAGE_FROM_SYSTEM |
+		FORMAT_MESSAGE_IGNORE_INSERTS, NULL, errcode, MAKELANGID(LANG_NEUTRAL,
+		SUBLANG_DEFAULT), buffer, (DWORD)bufferSize, NULL);
+	base_assert(rc);
+}
+
+#endif
---
+++ src/basic/err.h
@@ -0,0 +1,83 @@
+#ifndef ERR_INCLUDED_H
+#define ERR_INCLUDED_H
+
+#include <assert.h>
+#include <errno.h>
+#include <string.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+#ifdef WIN32
+#include "windows.h"
+#endif
+
+void safe_abort(const char *errMsg);
+
+#ifdef WIN32
+
+void win_error(char *buffer, size_t bufferSize);
+
+// Windows下的GetLastError样式的错误断言
+#define win_assert(x) \
+	do \
+	{ \
+		if (!(x)) \
+		{ \
+			char errStr[256]; \
+			win_error(errStr, 256); \
+			fprintf(stderr, "Assertion failed: %s (%s:%d)\n", errStr, __FILE__, __LINE__); \
+			safe_abort(errStr); \
+		} \
+	} while (false)
+
+#endif
+
+// 基本的断言，与assert功能类似
+#define base_assert(x) \
+	do \
+	{ \
+		if (!(x)) \
+		{ \
+			fprintf(stderr, "Assertion failed: %s (%s:%d)\n", #x, __FILE__, __LINE__); \
+			safe_abort(#x); \
+		} \
+	} while (false)
+
+// errno样式的错误断言
+#define errno_assert(x) \
+	do \
+	{ \
+		if (!(x)) \
+		{ \
+			const char *errStr = strerror(errno); \
+			fprintf(stderr, "%s (%s:%d)\n", errStr, __FILE__, __LINE__); \
+			safe_abort(errStr); \
+		} \
+	} while (false)
+
+// POSIX样式的错误断言
+#define posix_assert(x) \
+	do \
+	{ \
+		if (x) \
+		{ \
+			const char *errStr = strerror(x); \
+			fprintf(stderr, "%s (%s:%d)\n", errStr, __FILE__, __LINE__); \
+			safe_abort(errStr); \
+		} \
+	} while (false)
+
+
+// 内存分配失败的断言
+#define alloc_assert(x) \
+	do \
+	{ \
+		if (!(x)) \
+		{ \
+			fprintf(stderr, "FATAL ERROR: OUT OF MEMORY (%s:%d)\n", \
+			__FILE__, __LINE__); \
+			safe_abort("FATAL ERROR: OUT OF MEMORY"); \
+		}\
+	} while (false)
+
+#endif
